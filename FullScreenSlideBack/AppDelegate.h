b7fe94dd9aa3bc5bca5114dff3d262dce6eb8e75
crystal-wcyi <crystal_wcyi@163.com>
2015-08-11 23:06:33 +0800

    导航控制器全屏滑动返回效果

---
+++ FullScreenSlideBack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FullScreenSlideBack
+//
+//  Created by crystal on 15/8/11.
+//  Copyright (c) 2015年 crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FullScreenSlideBack/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  FullScreenSlideBack
+//
+//  Created by crystal on 15/8/11.
+//  Copyright (c) 2015年 crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
